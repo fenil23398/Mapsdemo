01bf49c98708a1c0d22a789420ce60e6910ba93e
fenil23398 <fenilshah23398@gmail.com>
2019-03-15 23:25:18 +0530

    changes initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { GoogleMaps } from '@ionic-native/google-maps';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -16,8 +16,11 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+  
+    GoogleMaps,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapOptions,
+} from '@ionic-native/google-maps';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+  map: GoogleMap;
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+  loadMap() {
+
+    // This code is necessary for browser
+    // Environment.setEnv({
+    //   'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
+    //   'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
+    // });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+  }
 }
