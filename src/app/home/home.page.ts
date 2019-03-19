ce6b9fe27eea271377155f61faac6cd348053b0a
fenil23398 <fenilshah23398@gmail.com>
2019-03-19 18:34:43 +0530

    changes done final map with all location showing & all clickable

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import {
   GoogleMaps,
   GoogleMap,
   GoogleMapOptions,
+  Marker,
+  GoogleMapsEvent
 } from '@ionic-native/google-maps/ngx';
+
 
 
 @Component({
@@ -12,22 +16,32 @@ import {
   styleUrls: ['home.page.scss'],
 })
 
-export class HomePage {
+ export class HomePage implements OnInit
+  {
+  
   map: GoogleMap;
+    
+  constructor(private platform:Platform){
 
-  ionViewDidLoad() {
-    console.log("Welcome to home page");
-    this.loadMap();
   }
+  
+  async ngOnInit() {
+    console.log("Welcome to home page");
+    await this.platform.ready();
+    await this.loadMap();
+   await  this.setplaces();
+  }
+ 
 
   loadMap() {
+    alert("hie from load map");
     let mapOptions: GoogleMapOptions = {
       camera: {
          target: {
-           lat: 43.0741904,
-           lng: -89.3809802
+           lat: 22.2587,
+           lng: 71.1924
          },
-         zoom: 18,
+         zoom: 7,
          tilt: 30
        }
     };
@@ -35,4 +49,45 @@ export class HomePage {
     this.map = GoogleMaps.create('map_canvas', mapOptions);
   }
 
+
+  setplaces()
+  {
+    this.map.addMarker({
+      position:{
+        lat:23.0225,
+        lng:72.5714
+      },
+      title:"ahemdabad",
+      disableAutoPan:true
+    }).then(this.onMarkerAdded);
+
+    
+    this.map.addMarker({
+      position:{
+        lat:21.1702,
+        lng:72.8311
+      },
+      title:"Surat",
+      disableAutoPan:true
+    }).then(this.onMarkerAdded);
+
+    
+    this.map.addMarker({
+      position:{
+        lat:22.3072,
+        lng:73.1812
+      },
+      title:"broda",
+      disableAutoPan:true
+    }).then(this.onMarkerAdded);
+  }
+
+  onMarkerAdded(marker :Marker){
+    marker.addEventListener(GoogleMapsEvent.MARKER_CLICK).subscribe(()=>{
+      alert("Marker" +marker.getTitle() + " clicked");
+    });
+  }
+
 }
+
+
